caed3f30a5f6dfdb72c5454d856491fa0c665b8e
almirrps <almirrps@hotmail.com>
2025-01-24 17:07:44 -0300

    Criando usecase e port de insercao do cliente

---
+++ src/main/java/com/santos/hexagonal/application/core/usecase/InsertCustomerUseCase.java
@@ -0,0 +1,26 @@
+package com.santos.hexagonal.application.core.usecase;
+
+import com.santos.hexagonal.application.core.domain.Customer;
+import com.santos.hexagonal.application.ports.out.FindAddressByZipCodeOutputPort;
+import com.santos.hexagonal.application.ports.out.InsertCustomerOutputPort;
+
+public class InsertCustomerUseCase {
+
+    private final FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort;
+
+    private final InsertCustomerOutputPort insertCustomerOutputPort;
+
+    public InsertCustomerUseCase(FindAddressByZipCodeOutputPort findAddressByZipCodeOutputPort,
+                                 InsertCustomerOutputPort insertCustomerOutputPort) {
+        this.findAddressByZipCodeOutputPort = findAddressByZipCodeOutputPort;
+        this.insertCustomerOutputPort = insertCustomerOutputPort;
+    }
+
+    public void insert(Customer customer, String zipCode) {
+        var address = findAddressByZipCodeOutputPort.find(zipCode);
+
+        customer.setAddress(address);
+        insertCustomerOutputPort.insert(customer);
+    }
+
+}
