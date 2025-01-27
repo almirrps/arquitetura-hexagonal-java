dd6f92bea6b8cfaa167446c9ec047125782c4452
almirrps <almirrps@hotmail.com>
2025-01-27 17:59:03 -0300

    Criando adapter e mapper para insercao de clientes

---
+++ src/main/java/com/santos/hexagonal/adapters/out/InsertCustomerAdapter.java
@@ -0,0 +1,25 @@
+package com.santos.hexagonal.adapters.out;
+
+import com.santos.hexagonal.adapters.out.repository.CustomerRepository;
+import com.santos.hexagonal.adapters.out.repository.mapper.CustomerEntityMapper;
+import com.santos.hexagonal.application.core.domain.Customer;
+import com.santos.hexagonal.application.ports.out.InsertCustomerOutputPort;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InsertCustomerAdapter implements InsertCustomerOutputPort {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerEntityMapper customerEntityMapper;
+
+    @Override
+    public void insert(Customer customer) {
+        var customerEntity = customerEntityMapper.toCustomerEntity(customer);
+        customerRepository.save(customerEntity);
+    }
+
+}
