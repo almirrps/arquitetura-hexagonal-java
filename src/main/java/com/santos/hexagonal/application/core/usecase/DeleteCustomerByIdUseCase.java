98928e8a36c9afbf39def05f31ce24c11c37b018
almirrps <almirrps@hotmail.com>
2025-01-28 16:32:07 -0300

    Criado usecase/output para deletar cliente

---
+++ src/main/java/com/santos/hexagonal/application/core/usecase/DeleteCustomerByIdUseCase.java
@@ -0,0 +1,23 @@
+package com.santos.hexagonal.application.core.usecase;
+
+import com.santos.hexagonal.application.ports.in.FindCustomerByIdInputPort;
+import com.santos.hexagonal.application.ports.out.DeleteCustomerByIdOutputPort;
+
+public class DeleteCustomerByIdUseCase {
+
+    private final FindCustomerByIdInputPort findCustomerByIdInputPort;
+
+    private final DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort;
+
+    public DeleteCustomerByIdUseCase(FindCustomerByIdInputPort findCustomerByIdInputPort,
+                                     DeleteCustomerByIdOutputPort deleteCustomerByIdOutputPort) {
+        this.findCustomerByIdInputPort = findCustomerByIdInputPort;
+        this.deleteCustomerByIdOutputPort = deleteCustomerByIdOutputPort;
+    }
+
+    public void delete(String id) {
+        findCustomerByIdInputPort.find(id);
+        deleteCustomerByIdOutputPort.delete(id);
+    }
+
+}
