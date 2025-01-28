070630f09d674897c1d5b22a433eb93a2a759e87
almirrps <almirrps@hotmail.com>
2025-01-28 16:00:46 -0300

    Criado usecase/output para atualizar cliente

---
+++ src/main/java/com/santos/hexagonal/application/ports/out/UpdateCustomerOutputPort.java
@@ -0,0 +1,9 @@
+package com.santos.hexagonal.application.ports.out;
+
+import com.santos.hexagonal.application.core.domain.Customer;
+
+public interface UpdateCustomerOutputPort {
+
+    void update(Customer customer);
+
+}
