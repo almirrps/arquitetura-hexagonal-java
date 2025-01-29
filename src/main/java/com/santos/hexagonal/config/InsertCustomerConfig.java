5757ea01147ec14f020e98e2192cd0686ca754b9
almirrps <almirrps@hotmail.com>
2025-01-29 14:39:36 -0300

    Implementado o envio de cpf ao Kafka

--- src/main/java/com/santos/hexagonal/config/InsertCustomerConfig.java
+++ src/main/java/com/santos/hexagonal/config/InsertCustomerConfig.java
@@ -2,6 +2,7 @@ package com.santos.hexagonal.config;
 
 import com.santos.hexagonal.adapters.out.FindAddressByZipCodeAdapter;
 import com.santos.hexagonal.adapters.out.InsertCustomerAdapter;
+import com.santos.hexagonal.adapters.out.SendCpfValidationAdapter;
 import com.santos.hexagonal.application.core.usecase.InsertCustomerUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,8 +12,9 @@ public class InsertCustomerConfig {
 
     @Bean
     public InsertCustomerUseCase insertCustomerUseCase(FindAddressByZipCodeAdapter findAddressByZipCodeAdapter,
-                                                       InsertCustomerAdapter insertCustomerAdapter) {
-        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter);
+                                                       InsertCustomerAdapter insertCustomerAdapter,
+                                                       SendCpfValidationAdapter sendCpfValidationAdapter) {
+        return new InsertCustomerUseCase(findAddressByZipCodeAdapter, insertCustomerAdapter, sendCpfValidationAdapter);
     }
 
 }
