5001ec1d1323e652ea2407b95c87a40aa67d710d
almirrps <almirrps@hotmail.com>
2025-01-31 15:52:41 -0300

    Implementada validacao dos sufixos em seus devidos pacotes

--- src/test/java/com/santos/hexagonal/architecture/NamingConventionTest.java
+++ src/test/java/com/santos/hexagonal/architecture/NamingConventionTest.java
@@ -9,6 +9,7 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 @AnalyzeClasses(packages = "com.santos.hexagonal")
 public class NamingConventionTest  {
 
+    //////******Realizando testes que garante que as classes estejam nos pacotes corretos******\\\\\\
     @ArchTest
     public static final ArchRule consumer_reside_only_consumer_package = classes()
             .that()
@@ -120,5 +121,106 @@ public class NamingConventionTest  {
             .should()
             .resideInAnyPackage("..hexagonal.config")
             .as("Config classes should reside inside config package in hexagonal package");
+
+    //////******Realizando testes que garante que os pacotes tenham somente um determinado sufixo******\\\\\\
+    @ArchTest
+    public static final ArchRule should_be_suffixed_consumer = classes()
+            .that()
+            .resideInAPackage("..consumer")
+            .should()
+            .haveSimpleNameEndingWith("Consumer");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_mapper = classes()
+            .that()
+            .resideInAPackage("..mapper")
+            .should()
+            .haveSimpleNameEndingWith("Mapper")
+            .orShould()
+            .haveSimpleNameEndingWith("MapperImpl");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_message = classes()
+            .that()
+            .resideInAPackage("..message")
+            .should()
+            .haveSimpleNameEndingWith("Message");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_controller = classes()
+            .that()
+            .resideInAPackage("..controller")
+            .should()
+            .haveSimpleNameEndingWith("Controller");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_request = classes()
+            .that()
+            .resideInAPackage("..request")
+            .should()
+            .haveSimpleNameEndingWith("Request");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_response = classes()
+            .that()
+            .resideInAPackage("..response")
+            .should()
+            .haveSimpleNameEndingWith("Response");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_client = classes()
+            .that()
+            .resideInAPackage("..client")
+            .should()
+            .haveSimpleNameEndingWith("Client");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_repository = classes()
+            .that()
+            .resideInAPackage("..repository")
+            .should()
+            .haveSimpleNameEndingWith("Repository");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_entity = classes()
+            .that()
+            .resideInAPackage("..entity")
+            .should()
+            .haveSimpleNameEndingWith("Entity");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_adapter = classes()
+            .that()
+            .resideInAPackage("..adapters.out")
+            .should()
+            .haveSimpleNameEndingWith("Adapter");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_usecase = classes()
+            .that()
+            .resideInAPackage("..usecase")
+            .should()
+            .haveSimpleNameEndingWith("UseCase");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_input_port = classes()
+            .that()
+            .resideInAPackage("..ports.in")
+            .should()
+            .haveSimpleNameEndingWith("InputPort");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_output_port = classes()
+            .that()
+            .resideInAPackage("..ports.out")
+            .should()
+            .haveSimpleNameEndingWith("OutputPort");
+
+    @ArchTest
+    public static final ArchRule should_be_suffixed_config = classes()
+            .that()
+            .resideInAPackage("..config")
+            .should()
+            .haveSimpleNameEndingWith("Config");
 
 }
